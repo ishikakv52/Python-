a72059943f4ee38962bb9439f0766ce7d4c3f1f3
Ishika <ishikakv52@gmail.com>
2026-08-18 16:27:29 +0530

    datatype and print

---
+++ basic/datatype.py
@@ -0,0 +1,8 @@
+#premitive : int,float,str,bool
+#Non-premitive : list,tuple,dictionary,sets,class
+
+
+a=100.11
+b="Hello"
+print(type(a))
+print(type(b))
---
+++ basic/print.py
@@ -0,0 +1,8 @@
+print('Jaat')
+print("Jaat")
+print('''Jaat''')
+print(100+200*4)
+#to print variable
+
+c=2006
+print(c)
