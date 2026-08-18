090f3dcc9bac1deee35921a8060aad10be6f9f77
Ishika <ishikakv52@gmail.com>
2026-08-18 16:25:58 +0530

    Implement user input for various data types
    Add input handling for string, int, float, and eval types.

---
+++ basic/input.py
@@ -0,0 +1,26 @@
+# *** string***#
+
+print("Enter the value")
+name=input()
+print(name)
+print(type(name))
+
+print("Enter any number")
+number=input()
+print(number)
+print(type(number))
+
+
+#*** int,float,eval ***#
+
+a=int(input("Enter any number : "))
+b=float(input("Enter any number : "))
+c=eval(input("Enter any number : "))
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
