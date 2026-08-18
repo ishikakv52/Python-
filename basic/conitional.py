2e143ed4c838b49f0519fe22096582e222e14e63
Ishika <ishikakv52@gmail.com>
2026-08-18 16:28:20 +0530

    OOPS conditional

---
+++ basic/conitional.py
@@ -0,0 +1,9 @@
+class C:
+    def oddeven(self):
+        n=int(input("Enter number : "))
+        if n%2==0:
+            print("Even")
+        else:
+            print("Odd")    
+c=C()
+c.oddeven()            
