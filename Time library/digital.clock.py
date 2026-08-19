daa896b37906b62819c47a6a849d58b8504f9cbe
Ishika <ishikakv52@gmail.com>
2026-08-19 11:31:51 +0530

    digital clock and typing game

---
+++ Time library/digital.clock.py
@@ -0,0 +1,9 @@
+import time
+
+while True:
+    print(time.strftime("%H:%M:%S"))
+    time.sleep(1)
+
+    # inp=input("Enter (exit) to stop the clock : ")
+    # if  inp=="exit":
+    #    break
---
+++ Time library/typing.game.py
@@ -0,0 +1,7 @@
+import time 
+input("Press enter/return when you are ready")
+start=time.time()
+
+input("To Type : Python  \n")
+end=time.time()
+print("Typing time : ",start-end," sec")
