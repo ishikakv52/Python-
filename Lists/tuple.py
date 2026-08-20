33ace4c7687af844d76e8aeebef44885087027fa
Ishika <ishikakv52@gmail.com>
2026-08-20 11:21:51 +0530

    Create tuple.py for tuple and list examples
    Added a script to demonstrate tuple and list conversions in Python.

---
+++ Lists/tuple.py
@@ -0,0 +1,22 @@
+x=(10,"A",3,5)
+print(x)
+
+y=list(x)
+print(y)
+# # z=type(x)
+# # print(z)
+# # a=type(y)
+# # print(a)
+
+
+# y.append("B")
+# print(y)
+
+# x=tuple(y)
+# print(x)
+
+
+# x=(1,2,3)
+# z=list(x)
+# z.remove(1)
+# print()
