0c3ec8a360a0af95dd5776dd88d6de85b6097921
Ishika <ishikakv52@gmail.com>
2026-08-21 12:55:59 +0530

    Add function to compare two numbers and print result

---
+++ Functions/greater.py
@@ -0,0 +1,8 @@
+def greater():
+    a=eval(input("Enter first number : "))
+    b=eval(input("Enter second number : "))
+    if a>b:
+        print("A is greater")
+    else:
+        print("B is greater")
+greater()
