679a66be589eb3112a673774b08e5c919494339d
Ishika <ishikakv52@gmail.com>
2026-08-21 12:57:11 +0530

    Implement addition and subtraction functions
    Added functions for subtraction and addition with user input.

---
+++ Functions/TSRN.py
@@ -0,0 +1,15 @@
+def sub(a,b):
+   c=a-b
+   print("Sub : " ,c)
+a=eval(input("Enter first number : "))
+b=eval(input("Enter second number : "))   
+sub(a,b)
+
+
+def add(x,y):
+   z=x+y
+   print("Sum : ",z)
+print("To sum: ")   
+x=eval(input("Enter first number : "))
+y=eval(input("Enter second number : "))      
+add(x,y)
