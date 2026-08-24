7158104d19b91c78a79117c38387ef7ffffac2b4
Ishika <ishikakv52@gmail.com>
2026-08-24 11:23:51 +0530

    Add dictionary functionality in dict.py

---
+++ dictionary/dict.py
@@ -0,0 +1,25 @@
+records={
+    "Name":"Ishika",
+    "Phone":999,
+    "Marks":11.9
+}
+print(type(records))
+print(records)
+
+
+
+Records={
+    "Names":["Rathi","Choudhary","Jaat"],
+    "Branch":["CS","IT","Civil"],
+    "Rollno.":[10,20,30]
+
+}
+
+print(Records)
+
+
+#to replace any value of the key
+print(records["Name"])
+d=eval(input("Enter data: "))
+records["Name"]=d
+print(records)
