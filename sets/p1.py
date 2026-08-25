c04562236122bd1a9ed43cd7b42fa91f842463ac
Ishika <ishikakv52@gmail.com>
2026-08-25 11:41:08 +0530

    Implement duplicate removal from user input
    Added a script to collect user input and remove duplicates using a set.

---
+++ sets/p1.py
@@ -0,0 +1,6 @@
+# User se numbers input lo (space separated) aur set use karke duplicates remove karke output do.
+s=set()
+for i in range(5):
+    a=int(input("Enter value :"))
+    s.add(a)
+print("S : ",s)    
