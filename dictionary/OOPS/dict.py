13c441f0c9b9552e97e9cc82fcb064c7d08a6d36
Ishika <ishikakv52@gmail.com>
2026-08-25 11:33:30 +0530

    Create dict class with records printing method
    Implement a class 'dict' with a method to print records.

---
+++ dictionary/OOPS/dict.py
@@ -0,0 +1,11 @@
+class dict:
+    def d(self):
+        Records={
+    "Names":["Rathi","Choudhary","Jaat"],
+    "Branch":["CS","IT","Civil"],
+    "Rollno.":[10,20,30]
+
+   }
+        print(Records)
+d=dict()
+d.d()        
