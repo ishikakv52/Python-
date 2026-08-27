4db662fe73b5c2890dfb951de07aad96bb5b196e
Ishika <ishikakv52@gmail.com>
2026-08-27 12:26:04 +0530

    Add string replacement functionality in Replace.py

---
+++ Strings/Replace.py
@@ -0,0 +1,6 @@
+a='''I am Ishika Rathi
+Currently doing my graduation in IMR college'''
+c=input("Enter the old word : ")
+d=input("Enter the new word : ")
+b=a.replace(c,d)        
+print("New string : ",b)
