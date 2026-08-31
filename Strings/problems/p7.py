cc8cc3774a49b5baf3f988be087680455ad48bda
Ishika <ishikakv52@gmail.com>
2026-08-31 14:07:14 +0530

    Add word counting functionality in p7.py
    Implement word counting in a string input.

---
+++ Strings/problems/p7.py
@@ -0,0 +1,18 @@
+a=input("enter the string")
+d=input("Enter the word to be counted : ") 
+x=0
+if d in a:
+    for i in a:
+            
+        if i in a:
+            
+            x=x+1    
+else:
+    print("Not found")        
+print(x)    
+
+# my name is ishika rathi
+# i am currently pursuing bca from imr college ghaziabad
+# i had done my schooling from kendriya vidyalaya kamla nehru nagar ghaziabad
+# I am focusing on  improving my logic building in programming
+# My goal is to become a skilled software professional
