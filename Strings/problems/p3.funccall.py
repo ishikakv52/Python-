9d7f52e264c0159f10e1ba9b6c485da47e418fdd
Ishika <ishikakv52@gmail.com>
2026-08-31 14:05:41 +0530

    Implement function calls based on user input

---
+++ Strings/problems/p3.funccall.py
@@ -0,0 +1,10 @@
+import p2
+a=eval(input("Press 1 for upper case,2 for upper case ,3 for search: "))
+if a==1:
+    p2.forupper()
+elif a==2:
+    p2.forlower()
+elif a==3:
+    p2.forsearch()    
+else:
+    print("invalid input")        
